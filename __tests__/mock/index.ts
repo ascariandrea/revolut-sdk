2279b51602527d5a93d168eaadf49ef04d9fb985
Andrea Ascari <andrea.ascari@buildo.io>
2017-12-23 13:00:19 +0100

    Created mocked data for accounts

--- __tests__/mock/index.ts
+++ __tests__/mock/index.ts
@@ -1,9 +1,17 @@
 import * as nock from 'nock';
 import RevolutClient from '../../lib';
+import { account, accounts } from './accounts';
 import { counterparties, counterparty } from './counterparties';
 
 export default function server() {
   return nock(RevolutClient.SANDBOX_URL)
+  // Accounts
+  .get('/accounts/42')
+    .reply(200, account)
+  .get('/accounts')
+    .reply(200, accounts)
+
+  // Counterparties
   .get('/counterparties')
     .reply(200, counterparties)
   .post('/counterparty')

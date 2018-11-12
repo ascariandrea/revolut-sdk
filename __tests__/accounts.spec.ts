16e490bd96cfd7f47edb6866485c3d8db0a5bb0a
Andrea Ascari <dev.ascariandrea@gmail.com>
2018-11-12 13:07:44 +0100

    Updated test to deal with revolut client reader

--- __tests__/accounts.spec.ts
+++ __tests__/accounts.spec.ts
@@ -1,17 +1,17 @@
 import { Some } from 'fp-ts/lib/Option'
-import RevolutClient from '../lib'
+import { RevolutClient } from '../lib'
 import { Account } from '../lib/api/accounts'
 import runMock from './mock'
 
-let revolutClient: RevolutClient
+let revolut: RevolutClient
 beforeAll(() => {
   runMock()
-  revolutClient = new RevolutClient({ sandbox: true, apiKey: 'test-api-key' })
+  revolut = RevolutClient.run({ sandbox: true, apiKey: 'test-api-key' })
 })
 
 describe('Accounts', () => {
   it('Should get an account', async () => {
-    const account = await revolutClient.accounts.get('42').run()
+    const account = await revolut.accounts.get('42').run()
 
     expect(account.isRight()).toBe(true)
     expect((account.value as Some<Account>).isSome()).toBe(true)
@@ -21,7 +21,7 @@ describe('Accounts', () => {
   })
 
   it('Should get a list of accounts', async () => {
-    const accounts = await revolutClient.accounts.getAll().run()
+    const accounts = await revolut.accounts.getAll().run()
 
     expect(accounts.isRight()).toBe(true)
     expect((accounts.value as Some<Account[]>).value.length).toEqual(3)
